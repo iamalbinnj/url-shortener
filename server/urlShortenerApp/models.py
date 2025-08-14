80fa37bdcc72765aa7091a593e09a348ccbcb06d
Albin N J <100420656+iamalbinnj@users.noreply.github.com>
2025-08-14 11:42:13 +0530

    models created

--- server/urlShortenerApp/models.py
+++ server/urlShortenerApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class URL(models.Model):
+    long_url = models.URLField()
+    short_code = models.CharField(max_length=10, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.short_code} -> {self.long_url}"
